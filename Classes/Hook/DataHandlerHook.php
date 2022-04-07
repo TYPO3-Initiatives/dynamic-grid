59eb9fd35983e34a01f6a5a83cae63ba8727b6c9
Lidia Demin <typecat@gmx.net>
2022-04-07 17:30:45 +0200

    [TASK] Extract looping to find predecessor to own method
    Reduces duplicate code
    Increases readability

--- Classes/Hook/DataHandlerHook.php
+++ Classes/Hook/DataHandlerHook.php
@@ -48,119 +48,98 @@ class DataHandlerHook
             ['tx_dynamicgrid_grid'], // fields to select
             'pages', // from
             [ 'uid' => $pageUid] // where
-        )
-            ->fetchAssociative();
+        )->fetchAssociative();
 
         if (!empty($page['tx_dynamicgrid_grid'])) {
             $dynamicGridConfig = json_decode($page['tx_dynamicgrid_grid'], true);
+            $newEntity = ['name' => $table, 'identifier' => $newElementUid];;
+
             // Update dynamic grid configuration
             foreach ($dynamicGridConfig as $colPosKey => $columnConfig) {
                 if ((int)$columnConfig['colPos'] === $colPos) {
+
+                    $existingItemTargets = $this->findExistingItemInGrid($columnConfig, $existingElementUid);
+                    $targetContainer = $existingItemTargets['targetContainer'];
+                    $targetItem = $existingItemTargets['targetItem'];
+                    $targetEntity = $existingItemTargets['targetEntity'];
+
                     switch ($gridPlacement) {
                         case 'newRow':
+                            // TODO: Handle this case
                             break;
                         case 'newItemBelow':
-                            // Find row
-                            foreach ($columnConfig['containers'] as $containerKey => $container) {
-                                // Find column
-                                foreach ($container['items'] as $itemKey => $item) {
-                                    foreach ($item['entities'] as $entityKey => $entity) {
-                                        if ((int)$entity['identifier'] === $existingElementUid) {
-                                            // Place new entity at the correct position
-                                            // or simply append if only one entity exists so far
-                                            if (1 < \count($item['entities'])) {
-                                                $dynamicGridConfig[$colPosKey]['containers'][$containerKey]['items'][$itemKey]['entities'] = array_merge(
-                                                    array_slice($item['entities'], 0, $entityKey + 1, false),
-                                                    [['name' => $table, 'identifier' => $newElementUid]],
-                                                    array_slice($item['entities'], $entityKey + 1, count($item['entities']), false)
-                                                );
-                                            } else {
-                                                $dynamicGridConfig[$colPosKey]['containers'][$containerKey]['items'][$itemKey]['entities'][] =
-                                                    ['name' => $table, 'identifier' => $newElementUid];
-                                            }
-                                            break;
-                                        }
-                                    }
-                                }
+                            // Place new entity at the correct position
+                            // or simply append if only one entity exists so far
+                            if (1 < \count($dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'][$targetItem]['entities'])) {
+                                $dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'][$targetItem]['entities'] = array_merge(
+                                    array_slice(
+                                        $dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'][$targetItem]['entities'],
+                                        0,
+                                        $targetEntity + 1,
+                                        false
+                                    ),
+                                    [$newEntity],
+                                    array_slice(
+                                        $dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'][$targetItem]['entities'],
+                                        $targetEntity + 1,
+                                        count($dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'][$targetItem]['entities']),
+                                        false
+                                    )
+                                );
+                            } else {
+                                $dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'][$targetItem]['entities'][] = $newEntity;
                             }
                             break;
                         case 'newItemAbove':
-                            // Find row
-                            foreach ($columnConfig['containers'] as $containerKey => $container) {
-                                // Find column
-                                foreach ($container['items'] as $itemKey => $item) {
-                                    foreach ($item['entities'] as $entityKey => $entity) {
-                                        if ((int)$entity['identifier'] === $existingElementUid) {
-                                            // Check if the existing element is in the same row
-                                            if (isset($dynamicGridConfig[$colPosKey]['containers'][$containerKey]['items'][$itemKey + 1]['entities'])) {
-                                                // If yes, prepend new entity as new column in the next row
-                                                array_unshift(
-                                                    $dynamicGridConfig[$colPosKey]['containers'][$containerKey]['items'][$itemKey + 1]['entities'],
-                                                    ['name' => $table, 'identifier' => $newElementUid]
-                                                );
-                                            } else {
-                                                if (isset($dynamicGridConfig[$colPosKey]['containers'][$containerKey+1]['items'])) {
-                                                    // Otherwise, prepend new entity in the first column of next row
-                                                    array_unshift(
-                                                        $dynamicGridConfig[$colPosKey]['containers'][$containerKey+1]['items'][0]['entities'],
-                                                        ['name' => $table, 'identifier' => $newElementUid]
-                                                    );
-                                                }
-                                            }
-                                            break;
-                                        }
-                                    }
+                            // Check if the existing element is in the same row
+                            if (isset($dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'][$targetItem + 1]['entities'])) {
+                                // If yes, prepend new entity as new column in the next row
+                                array_unshift(
+                                    $dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'][$targetItem + 1]['entities'],
+                                    $newEntity
+                                );
+                            } else {
+                                if (isset($dynamicGridConfig[$colPosKey]['containers'][$targetContainer + 1]['items'])) {
+                                    // Otherwise, prepend new entity in the first column of next row
+                                    array_unshift(
+                                        $dynamicGridConfig[$colPosKey]['containers'][$targetContainer + 1]['items'][0]['entities'],
+                                        $newEntity
+                                    );
                                 }
                             }
                             break;
                         case 'newColumnLeft':
-                            // Find row
-                            foreach ($columnConfig['containers'] as $containerKey => $container) {
-                                // Find column
-                                foreach ($container['items'] as $itemKey => $item) {
-                                    foreach ($item['entities'] as $entityKey => $entity) {
-                                        if ((int)$entity['identifier'] === $existingElementUid) {
-                                            // Prepend new entity as new column in the next row
-                                            array_unshift(
-                                                $dynamicGridConfig[$colPosKey]['containers'][$containerKey + 1 ]['items'],
-                                                [
-                                                    'size' => 1,
-                                                    'entities' => [
-                                                        ['name' => $table, 'identifier' => $newElementUid]
-                                                    ]
-                                                ]
-                                            );
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
+                            // Prepend new entity as new column in the next row
+                            array_unshift(
+                                $dynamicGridConfig[$colPosKey]['containers'][$targetContainer + 1 ]['items'],
+                                [
+                                    'size' => 1,
+                                    'entities' => [$newEntity]
+                                ]
+                            );
                             break;
                         case 'newColumnRight':
-                            // Find row
-                            foreach ($columnConfig['containers'] as $containerKey => $container) {
-                                // Find column
-                                foreach ($container['items'] as $itemKey => $item) {
-                                    foreach ($item['entities'] as $entityKey => $entity) {
-                                        if ((int)$entity['identifier'] === $existingElementUid) {
-                                            // Place new entity as new column
-                                            $dynamicGridConfig[$colPosKey]['containers'][$containerKey]['items'] = array_merge(
-                                                array_slice($container['items'], 0, $itemKey + 1, false),
-                                                [
-                                                    [
-                                                        'size' => 1,
-                                                        'entities' => [
-                                                            ['name' => $table, 'identifier' => $newElementUid]
-                                                        ]
-                                                    ]
-                                                ],
-                                                array_slice($container['items'], $itemKey + 1, count($container['items']), false)
-                                            );
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
+                            // Place new entity as new column
+                            $dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'] = array_merge(
+                                array_slice(
+                                    $dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'],
+                                    0,
+                                    $targetItem + 1,
+                                    false
+                                ),
+                                [
+                                    [
+                                        'size' => 1,
+                                        'entities' => [$newEntity]
+                                    ]
+                                ],
+                                array_slice(
+                                    $dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items'],
+                                    $targetItem + 1,
+                                    count($dynamicGridConfig[$colPosKey]['containers'][$targetContainer]['items']),
+                                    false
+                                )
+                            );
                             break;
                         default:
                             break;
@@ -176,5 +155,42 @@ class DataHandlerHook
                 ['uid' => $pageUid] // where
             );
         }
+    }
+
+    /**
+     * Find predecessor of the new content element in the grid
+     *
+     * @param array $columnConfig Dynamic grid config of target colPos
+     * @param int $existingElementUid Uid of predecessor of the new content element
+     *
+     * @return array Array of target keys in the dynamic grid
+     */
+    protected function findExistingItemInGrid(array $columnConfig, int $existingElementUid): array
+    {
+        $targetContainer = 0;
+        $targetItem = 0;
+        $targetEntity = 0;
+
+        // Find row
+        foreach ($columnConfig['containers'] as $containerKey => $container) {
+            // Find column
+            foreach ($container['items'] as $itemKey => $item) {
+                // Find item
+                foreach ($item['entities'] as $entityKey => $entity) {
+                    if ((int)$entity['identifier'] === $existingElementUid) {
+                        $targetContainer = $containerKey;
+                        $targetItem = $itemKey;
+                        $targetEntity = $entityKey;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return [
+            'targetContainer' => $targetContainer,
+            'targetItem' => $targetItem,
+            'targetEntity' => $targetEntity
+        ];
     }
 }
